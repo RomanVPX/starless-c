1a3783f23f4fe0800905433cf69058d0433fa72d
RomanVPX <dev@romanvpx.xyz>
2025-11-28 17:18:21 +0300

    Updated blackbody ramp and set it as a default
    Calculated using CIE 1964 10-Degree Standard Observer illuminants, looks a tiny bit warmer.

--- src/config_default_paths.h
+++ src/config_default_paths.h
@@ -2,7 +2,7 @@
 #define CONFIG_DEFAULTS_H
 
 // These values are used if the config file does not specify them:
-#define DEFAULT_BLACKBODY_RAMP_PATH "blackbody_ramp/blackbody_ramp_1000_50000K_4096_linear_srgb_normalized.ramp"
+#define DEFAULT_BLACKBODY_RAMP_PATH "blackbody_ramp/bb_ramp_1000_50000K_8192_linear_srgb_normalized.ramp"
 #define DEFAULT_DISK_TEXTURE_PATH "textures/adisk.jpg"
 #define DEFAULT_SKY_TEXTURE_PATH "textures/bgedit.jpg"
 
